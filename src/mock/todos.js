7ae5a227b52804e0a40f24e3628bbec974546938
DanielGrajales-U <dani22.24gra@gmail.com>
2023-10-20 20:52:08 -0500

    Update clean todolist

--- src/mock/todos.js
+++ src/mock/todos.js
@@ -1,7 +1,4 @@
 const todos = [
-    {id:1,name:"Aprender Vue3", complete:false},
-    {id:2,name:"Aprender Patrones de diseño", complete:false},
-    {id:3,name:"Aprender NodeJs", complete:true},
 ];
 
 export default todos;
